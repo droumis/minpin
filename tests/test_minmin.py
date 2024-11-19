6fd2f8a9aba6811c3ca8ed0745f53ea73a3b1f11
Demetris Roumis <roumis.d@gmail.com>
2024-11-18 17:53:36 -0800

    format code

--- tests/test_minmin.py
+++ tests/test_minmin.py
@@ -18,8 +18,9 @@ def run_minpin_forgiving(input_yaml, expected_changes):
         tmp.write(input_yaml)
         tmp.flush()
 
-        with patch("minpin.minpin.get_conda_list", return_value=mock_conda_list()), \
-             patch("minpin.minpin.get_pip_list", return_value={}):
+        with patch(
+            "minpin.minpin.get_conda_list", return_value=mock_conda_list()
+        ), patch("minpin.minpin.get_pip_list", return_value={}):
             main([tmp.name])  # Call the `main` function with the test argument
 
         with open(tmp.name, "r") as result_file:
@@ -46,7 +47,7 @@ dependencies:
     expected_changes = [
         r"python>=3\.11\.10.*# auto min pinned.*# blah blah",  # Matches with the auto min pinned tag
         r"openssl>=3\.3\.0.*# blah",  # Pre-pinned package preserved
-        r"setuptools>=75\.0\.0",     # Pre-pinned package preserved
+        r"setuptools>=75\.0\.0",  # Pre-pinned package preserved
     ]
     result = run_minpin_forgiving(input_yaml, expected_changes)
     print(result)
